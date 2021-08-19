9917e88895bdcffca2e0288f2ba68d8afb4e3f5c
Rajni Dua <rajni.dua14@gmail.com>
2021-08-19 10:23:42 +0930

    fixed the wild card issue

--- Develop/server.js
+++ Develop/server.js
@@ -22,6 +22,11 @@ app.get('/notes', (req, res) =>
 );
 
 
+// Wildcard route to direct users to an index html page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
